e584039de876351853fdd50bc75632923ddd4295
RivkaRSagi <rivka.r.sagi@gmail.com>
2025-02-10 21:44:30 -0500

    fix bugs

--- jeopardy_source/players.c
+++ jeopardy_source/players.c
@@ -14,7 +14,7 @@
 bool player_exists(player *players, int num_players, char *name)
 {
     for (int i=0; i<num_players; i++) {
-        if (strcmp(players[i].name, name) == 0) { 
+        if (strcmp(*players[i].name, name) == 0) { 
             return true;
         }
     }
