1e056080a96c6d5022bc5e806712b15c473bc96c
RivkaRSagi <rivka.r.sagi@gmail.com>
2025-02-10 22:02:44 -0500

    fix bugs

--- jeopardy_source/players.c
+++ jeopardy_source/players.c
@@ -26,7 +26,7 @@ bool player_exists(player *players, int num_players, char *name)
 void update_score(player *players, int num_players, char *name, int score)
 {
     for (int i=0; i<num_players; i++){
-        if (strncasecmp(players[i].name, name) == 0){
+        if (strncasecmp(players[i].name, name, strlen(name)) == 0){
             players[i].score = score;
             return 0;
         }
